c0fd8b17c87125f087ced7cb9a3aa57b610eb2cb
James Scott <james.scott@mccombs.utexas.edu>
2017-02-13 09:36:27 -0600

    no message

---
+++ rscripts/LifeExpectancy.R
@@ -0,0 +1,44 @@
+library(mosaic)
+
+plot(LifeExp ~ PPGDP, data=LifeExpectancy)
+
+# A log transformation of GDP?
+hist(LifeExpectancy$PPGDP)
+
+# Still looks nonlinear
+plot(LifeExp ~ log(PPGDP), data=LifeExpectancy)
+
+# What about stratifying by group?
+xyplot(LifeExp ~ log(PPGDP) | Group, data=LifeExpectancy)
+
+# Use a model with dummy variables
+lm1 = lm(LifeExp ~ log(PPGDP) + Group, data=LifeExpectancy)
+
+plotModel(lm1)
+
+# On the transformed scale
+plot(LifeExp ~ log(PPGDP), data=LifeExpectancy)
+points(fitted(lm1) ~ log(PPGDP), data=LifeExpectancy,
+      col='blue', pch=19)
+
+# Prediction interval
+new_data = data.frame(PPGDP = 20000, Group ='oecd')
+predict(lm1, new_data, interval='prediction', level = 0.95)
+
+
+## Bootstrapping
+
+
+# "By hand"
+sigmae = sd(resid(lm1))
+
+coef(lm1)
+
+# prediction for OECD country with GDP = 20000
+yhat = 37.35888 + log(20000)* 3.17732 + 12.17037
+yhat + sigmae
+yhat - sigmae
+
+# coverage
+sum(abs(resid(lm1)) > sigmae)
+1 - 43/199
