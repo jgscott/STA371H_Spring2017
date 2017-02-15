edf036025637534dee38b4d854cc5c6bc21dc2a8
James Scott <james.scott@mccombs.utexas.edu>
2017-02-15 08:13:40 -0600

    Life expectancy

--- rscripts/LifeExpectancy.R
+++ rscripts/LifeExpectancy.R
@@ -1,4 +1,7 @@
 library(mosaic)
+source('http://jgscott.github.io/teaching/r/utils/class_utils.R')
+
+LifeExpectancy = read.csv('../data/LifeExpectancy.csv')
 
 plot(LifeExp ~ PPGDP, data=LifeExpectancy)
 
@@ -14,31 +17,47 @@ xyplot(LifeExp ~ log(PPGDP) | Group, data=LifeExpectancy)
 # Use a model with dummy variables
 lm1 = lm(LifeExp ~ log(PPGDP) + Group, data=LifeExpectancy)
 
-plotModel(lm1)
+plotModel(lm1, pch=19)
+
+# With an interaction? Doesn't look necessary
+lm2 = lm(LifeExp ~ log(PPGDP) + Group + log(PPGDP):Group, data=LifeExpectancy)
+simple_anova(lm2)
 
 # On the transformed scale
 plot(LifeExp ~ log(PPGDP), data=LifeExpectancy)
 points(fitted(lm1) ~ log(PPGDP), data=LifeExpectancy,
       col='blue', pch=19)
 
+plot(LifeExp ~ log(PPGDP), data=LifeExpectancy)
+points(fitted(lm2) ~ log(PPGDP), data=LifeExpectancy,
+      col='red', pch=19)
+
 # Prediction interval
 new_data = data.frame(PPGDP = 20000, Group ='oecd')
 predict(lm1, new_data, interval='prediction', level = 0.95)
 
-
 ## Bootstrapping
+boot1 = do(2500)*{
+	lm_boot = lm(LifeExp ~ log(PPGDP) + Group, data=resample(LifeExpectancy))
+	lm_boot
+}
+
+# First few lines
+head(boot1)
+
+# Confidence intervals
+confint(boot1)
+
+# standard errors (std deviation of each column
+sd_cols(boot1)  # this function is in class_utils.R script from website
 
 
-# "By hand"
-sigmae = sd(resid(lm1))
+# Using the normal linear regression model
+summary(lm1)
+confint(lm1)
+confint(boot1)  # compare
+coef_table(lm1)  # this function is in class_utils.R script from website
 
-coef(lm1)
+# Residuals look somewhat non-normal
+hist(resid(lm1), 20)
 
-# prediction for OECD country with GDP = 20000
-yhat = 37.35888 + log(20000)* 3.17732 + 12.17037
-yhat + sigmae
-yhat - sigmae
-
-# coverage
-sum(abs(resid(lm1)) > sigmae)
-1 - 43/199
